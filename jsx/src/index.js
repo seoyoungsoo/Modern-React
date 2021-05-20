7e8b6ec89692693adc5656c5fe59d2c0172922cd
seoyoungsoo <iulove37@naver.com>
2021-05-20 22:34:53 +0900

    MR Section 2

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,23 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
-}
+  const buttonText = 'Click me';
+  const labelText = 'Enter name:';
+
+  return (
+  <div>
+    <label className="label" htmlFor="name">
+      {labelText}
+    </label>
+    <input id="name" type="text" />
+    {/* 기존 html 방식과 jsx의 차이점 */}
+    {/* <button style="background-color: blue; color: white;">Submit</button> */}
+    <button style={{backgroundColor: 'blue', color:'white'}}>
+      {buttonText}
+    </button>
+  </div>
+  );
+};
 // Take the react component and show it on the screen
 ReactDOM.render(
   <App />,
