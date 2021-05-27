6eca989ff971e252f24489521e3b6ff6fa561571
seoyoungsoo <iulove37@naver.com>
2021-05-27 23:47:07 +0900

    MR Section 3

---
+++ components/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
