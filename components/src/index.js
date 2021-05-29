1cb96926393ea11fb7f8980141c7173811e75ea3
seoyoungsoo <iulove37@naver.com>
2021-05-30 01:04:52 +0900

    MR Section 3

--- components/src/index.js
+++ components/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <ApprovalCard />
+      <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" />
+      <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 2:00AM" content="I like the subject" />
+      <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing" />
     </div>
   );
 };
