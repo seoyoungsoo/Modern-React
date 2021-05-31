38ca036beda53c772a9c2e017eba81f7aff896c0
seoyoungsoo <iulove37@naver.com>
2021-06-01 00:32:04 +0900

    MR Section 3

--- components/src/index.js
+++ components/src/index.js
@@ -7,10 +7,24 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail avatar={faker.image.avatar()} author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post" />
+      </ApprovalCard>
+
+      <ApprovalCard>
       <CommentDetail avatar={faker.image.avatar()} author="Alex" timeAgo="Today at 2:00AM" content="I like the subject" />
+      </ApprovalCard>
+
+      <ApprovalCard>
       <CommentDetail avatar={faker.image.avatar()} author="Jane" timeAgo="Yesterday at 5:00PM" content="I like the writing" />
+      </ApprovalCard>
     </div>
   );
 };
